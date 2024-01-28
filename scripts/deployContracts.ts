ce8849b25eb644f657f3ff70a462de9c13c21c44
0xKarasu <0xKarasu@protonmail.com>
2024-01-28 16:56:55 +0100

    :rocket: update deploy script

--- scripts/deployContracts.ts
+++ scripts/deployContracts.ts
@@ -1,97 +1,138 @@
-import { Account, Provider, Contract, json, uint256, RpcProvider, constants } from "starknet";
+import {
+  Account,
+  Provider,
+  Contract,
+  json,
+  uint256,
+  RpcProvider,
+  constants,
+} from 'starknet';
 import fs from 'fs';
 import dotenv from 'dotenv';
-import { appendToEnv, readConfigs, writeConfigs } from "./utils";
+import { appendToEnv, readConfigs, writeConfigs } from './utils';
 
-dotenv.config({ path: __dirname + '/../.env' })
+dotenv.config({ path: __dirname + '/../.env' });
 
-const network = process.env.STARKNET_NETWORK || ''
+const network = process.env.STARKNET_NETWORK || '';
 
-const provider = new RpcProvider({ nodeUrl: network == "mainnet" ? constants.NetworkName.SN_MAIN : constants.NetworkName.SN_GOERLI });
-const owner = new Account(provider, process.env.ACCOUNT_ADDRESS as string, process.env.ACCOUNT_PK as string, "1");
+const provider = new RpcProvider({
+  nodeUrl:
+    network == 'mainnet'
+      ? constants.NetworkName.SN_MAIN
+      : constants.NetworkName.SN_GOERLI,
+});
+
+const owner = new Account(
+  provider,
+  process.env.ACCOUNT_ADDRESS as string,
+  process.env.ACCOUNT_PK as string,
+  '1'
+);
 
 async function deployPoolingManagerContract(): Promise<Contract> {
-    let contractAddress: any;
-    const compiledContract = await json.parse(fs.readFileSync(`./target/dev/nimbora_yields_PoolingManager.contract_class.json`).toString('ascii'));
-    let { transaction_hash, contract_address } = await owner.deploy({
-        classHash: process.env.POOLING_MANAGER_CLASS_HASH as string,
-        constructorCalldata: {
-            owner: owner.address,
-        },
-    });
-    [contractAddress] = contract_address;
-    // await provider.waitForTransaction(transaction_hash);
-    const poolingManagerContract = new Contract(compiledContract.abi, contractAddress, owner);
-    console.log('✅ Test PoolingManager contract connected at =', poolingManagerContract.address);
+  let contractAddress: any;
+  const compiledContract = await json.parse(
+    fs
+      .readFileSync(
+        `./target/dev/nimbora_yields_PoolingManager.contract_class.json`
+      )
+      .toString('ascii')
+  );
+  let config = readConfigs();
 
-    let configs = readConfigs()
-    if (!configs[network]) {
-        configs[network] = {}
-    }
-    configs[network].l2PoolingManager = poolingManagerContract.address
-    writeConfigs(configs)
-    return poolingManagerContract;
+  let { transaction_hash, contract_address } = await owner.deploy({
+    classHash: config[network].POOLING_MANAGER_CLASS_HASH as string,
+    constructorCalldata: {
+      owner: owner.address,
+    },
+  });
+  [contractAddress] = contract_address;
+  // await provider.waitForTransaction(transaction_hash);
+  const poolingManagerContract = new Contract(
+    compiledContract.abi,
+    contractAddress,
+    owner
+  );
+  console.log(
+    '✅ Test PoolingManager contract connected at =',
+    poolingManagerContract.address
+  );
+
+  let configs = readConfigs();
+  if (!configs[network]) {
+    configs[network] = {};
+  }
+  configs[network].l2PoolingManager = poolingManagerContract.address;
+  writeConfigs(configs);
+  return poolingManagerContract;
 }
 
 async function deployFactoryContract(): Promise<Contract> {
-    let contractAddress: any;
-    const compiledContract = await json.parse(fs.readFileSync(`./target/dev/nimbora_yields_Factory.contract_class.json`).toString('ascii'));
-    let configs = readConfigs();
-    const l2PoolingManager = configs[network].l2PoolingManager;
+  let contractAddress: any;
+  const compiledContract = await json.parse(
+    fs
+      .readFileSync(`./target/dev/nimbora_yields_Factory.contract_class.json`)
+      .toString('ascii')
+  );
+  let configs = readConfigs();
+  const l2PoolingManager = configs[network].l2PoolingManager;
 
-    let { transaction_hash, contract_address } = await owner.deploy({
-        classHash: process.env.FACTORY_CLASS_HASH as string,
-        constructorCalldata: {
-            pooling_manager: l2PoolingManager as string,
-            token_class_hash: process.env.TOKEN_CLASS_HASH as string,
-            token_manager_class_hash: process.env.TOKEN_MANAGER_CLASS_HASH as string,
-        },
-    });
-    [contractAddress] = contract_address;
-    // await provider.waitForTransaction(transaction_hash);
+  let { transaction_hash, contract_address } = await owner.deploy({
+    classHash: configs[network].FACTORY_CLASS_HASH as string,
+    constructorCalldata: {
+      pooling_manager: l2PoolingManager as string,
+      token_class_hash: configs[network].TOKEN_CLASS_HASH as string,
+      token_manager_class_hash: configs[network]
+        .TOKEN_MANAGER_CLASS_HASH as string,
+    },
+  });
+  [contractAddress] = contract_address;
+  // await provider.waitForTransaction(transaction_hash);
 
-    const factoryContract = new Contract(compiledContract.abi, contractAddress, owner);
-    console.log('✅ Test Factory contract connected at =', factoryContract.address);
+  const factoryContract = new Contract(
+    compiledContract.abi,
+    contractAddress,
+    owner
+  );
+  console.log(
+    '✅ Test Factory contract connected at =',
+    factoryContract.address
+  );
 
-    configs[network].factory = factoryContract.address
-    writeConfigs(configs)
+  configs[network].factory = factoryContract.address;
+  writeConfigs(configs);
 
-    return factoryContract;
+  return factoryContract;
 }
 
-
-
-
 async function main() {
+  const flag = process.argv[2];
+  const action = process.argv[3];
 
-    const flag = process.argv[2];
-    const action = process.argv[3];
+  if (!flag || !action) {
+    throw new Error('Missing --contract <contract_name>');
+  }
 
-    if (!flag || !action) {
-        throw new Error("Missing --contract <contract_name>");
+  if (flag == '--contract') {
+    switch (action) {
+      case 'PoolingManager':
+        console.log('Deploying PoolingManager...');
+
+        const poolingManagerContract = await deployPoolingManagerContract();
+        break;
+
+      case 'Factory':
+        console.log('Deploying Factory...');
+        const factoryContractAddress = await deployFactoryContract();
+
+        break;
     }
-
-    if (flag == "--contract") {
-        switch (action) {
-            case "PoolingManager":
-                console.log("Deploying PoolingManager...");
-
-                const poolingManagerContract = await deployPoolingManagerContract(
-                );
-                break;
-
-            case "Factory":
-                console.log("Deploying Factory...");
-                const factoryContractAddress = await deployFactoryContract();
-
-                break;
-        }
-    } else if (flag == "--setup") {
-        const contract_address = process.argv[4];
-        if (!contract_address) {
-            throw new Error("Error: Provide contract address");
-        }
+  } else if (flag == '--setup') {
+    const contract_address = process.argv[4];
+    if (!contract_address) {
+      throw new Error('Error: Provide contract address');
     }
+  }
 }
 
 main();
